61d515a71cd5e5659be900f8550976b2cf923916
Yuda <madeyudaadiwinata@gmail.com>
2023-11-26 22:32:19 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { RateComponent } from './review/rate/rate.component';
+import { ListComponent } from './review/list/list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/register', pathMatch: 'full' }, // Redirect to register on initial load
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  { path: 'rate/:id', component:RateComponent},
+  { path: 'review', component:ListComponent},
   // ... other routes if any
 ];
 
